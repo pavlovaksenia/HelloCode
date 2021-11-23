920ea81f3cc6a9e60517f9ee76d8e0ed19230579
pavlovaksenia <pavksan@gmail.com>
2021-11-23 21:32:22 +0700

    Example005_IfElse

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ксюша")
+{
+    Console.WriteLine("Ура, это же Ксюша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
