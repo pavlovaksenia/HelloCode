9fedc49947fce5df88497ede20620b895f3580d4
pavlovaksenia <pavksan@gmail.com>
2021-11-24 22:34:45 +0700

    Example010 Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿//ищем какой индекс у заданного числа
+int[] array = {1,2,3,4,8,5,6,7,8};
+
+int n = array.Length; //определяет длину массива
+int find = 8;
+
+int index = 0;
+
+while(index<n)
+{
+if (array[index]==find)
+{
+    Console.WriteLine (index);
+    break;
+}
+    index++; //index = index+1;
+}
